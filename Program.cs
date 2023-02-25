629f6c4df20e36c5200267ec08461fb1bd125287
RaminIsko <ramsmagast@gmail.com>
2023-02-25 20:17:44 +0600

    Вторая задача

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿/*
 Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
 Напишите программу, которая покажет количество чётных чисел в массиве.
-
 [345, 897, 568, 234] -> 2
 */
 int[] RandomArray (int length, int minValue, int maxValue){
@@ -31,3 +30,26 @@ int maxValue = int.Parse(Console.ReadLine()!);
 int[] array = RandomArray(length, minValue, maxValue);
 CountEvenElements(array);
 
+/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+void SumOfOddElements (int[] array){
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++){
+        if(i % 2 == 1){
+            sum += array[i];
+        }
+    }
+    Console.WriteLine($"Сумма элементов массива с нечетным индексом: {sum}");
+}
+Console.Write("Введите длину массива: ");
+length = int.Parse(Console.ReadLine()!);
+Console.Write("Введите минимальное значение элементов массива: ");
+minValue = int.Parse(Console.ReadLine()!);
+Console.Write("Введите максимальное значение элементов массива: ");
+maxValue = int.Parse(Console.ReadLine()!);
+int[] array2 = RandomArray(length, minValue, maxValue);
+SumOfOddElements(array2);
